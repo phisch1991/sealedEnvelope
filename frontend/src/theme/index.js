63fdd7ab2a2815a4dc3131c374112958d8f3c289
Philipp <phisch1991@web.de>
2021-04-18 16:56:53 +0200

    Allow Envelope sending & receiving via url

--- frontend/src/theme/index.js
+++ frontend/src/theme/index.js
@@ -1,8 +1,7 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles'
 
 const color = '#61DAFB'
 const backgroundColor = '#282c34'
-
 
 export default createMuiTheme({
   palette: {
@@ -26,29 +25,32 @@ export default createMuiTheme({
         border: `solid ${backgroundColor} 2px`,
         margin: '5px',
         backgroundColor: color,
-        "&:hover": {
+        '&:hover': {
           color: color,
           backgroundColor: backgroundColor,
           border: `solid ${color} 2px`,
-
-        }
+        },
       },
-
     },
     MuiInput: {
       input: {
         color: color, // if you also want to change the color of the input, this is the prop you'd use,
       },
       underline: {
-        "&:before": {
-          borderBottomColor: color
-        }
-      }
+        '&:before': {
+          borderBottomColor: color,
+        },
+      },
     },
     MuiInputLabel: {
       root: {
-        color: color
-      }
-    }
-  }
-});
+        color: color,
+      },
+    },
+    Link: {
+      root: {
+        backgroundColor: '#FFFFFF',
+      },
+    },
+  },
+})
